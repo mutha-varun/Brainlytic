17eecc0f862e83413567f836aeb18cf43a477221
Varun Mutha <muthavarun07@gmail.com>
2026-07-05 12:26:33 +0530

    feat(Theme): All the colors and themes used across the app

--- brainlytic/lib/core/theme/pallete.dart
+++ brainlytic/lib/core/theme/pallete.dart
@@ -2,5 +2,14 @@ import 'package:flutter/material.dart';
 
 class Pallete {
   static Color scaffoldColor = Color.fromRGBO(18, 18, 18, 1);
-  static Color borderColor = Colors.grey;
+  static Color enabledBorderColor = Colors.grey.shade400;
+  static Color focusedBorderColor = Colors.indigoAccent.shade400;
+  static Color disabledBorderColor = Colors.grey.shade600;
+  static Color seedColor = Color(0xFFFF9100);
+  static Color blackColor = Colors.black;
+  static Color transparentColor = Colors.transparent;
+  static Color indigo700Color =Colors.indigoAccent.shade700;
+  static Color correctAnsColor = Colors.green;
+  static Color wrongAnsColor = Colors.red;
+
 }
