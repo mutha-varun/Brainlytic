32d7c7f41cc5aa8eb1f8bc62301139861f601791
Varun Mutha <muthavarun07@gmail.com>
2026-07-15 22:18:13 +0530

    Feat: Added bloc to root of the widget

--- brainlytic/lib/main.dart
+++ brainlytic/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:brainlytic/core/router/app_router.dart';
 import 'package:brainlytic/core/theme/theme.dart';
+import 'package:brainlytic/features/auth/data/datasources/firebase_auth_datasources.dart';
+import 'package:brainlytic/features/auth/data/repository/auth_repository_impl.dart';
+import 'package:brainlytic/features/auth/domain/usecases/register_usecase.dart';
+import 'package:brainlytic/features/auth/pages/bloc/auth_bloc.dart';
 import 'package:brainlytic/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -27,22 +32,35 @@ class Brainlytic extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: "Brainlytic",
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme.darkTheme,
-      // home: StreamBuilder(
-      //   stream: FirebaseAuth.instance.authStateChanges(),
-      //   builder: (context, snapshot) {
-          
-      //     if(snapshot.data != null){
-      //       return HomeScreen();
-      //     }
-      //     return Onboarding();
-      //   },
-      // )
-
-      routerConfig: appRouter,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthBloc>(
+          create: (_)=>AuthBloc(
+            registerUsecase: RegisterUsecase(
+              AuthRepositoryImpl(
+                FirebaseAuthDatasources()
+              )
+            )
+          )
+        )
+      ],
+      child: MaterialApp.router(
+        title: "Brainlytic",
+        debugShowCheckedModeBanner: false,
+        theme: AppTheme.darkTheme,
+        // home: StreamBuilder(
+        //   stream: FirebaseAuth.instance.authStateChanges(),
+        //   builder: (context, snapshot) {
+            
+        //     if(snapshot.data != null){
+        //       return HomeScreen();
+        //     }
+        //     return Onboarding();
+        //   },
+        // )
+      
+        routerConfig: appRouter,
+      ),
     );
   }
 }
