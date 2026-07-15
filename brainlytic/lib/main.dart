55ce3ee68e59f3c7bae41d4b1c8bdc3cd0e41248
Varun Mutha <muthavarun07@gmail.com>
2026-07-15 23:16:33 +0530

    Refactor: Moved the app into a file away from main

--- brainlytic/lib/main.dart
+++ brainlytic/lib/main.dart
@@ -1,13 +1,7 @@
-import 'package:brainlytic/core/router/app_router.dart';
-import 'package:brainlytic/core/theme/theme.dart';
-import 'package:brainlytic/features/auth/data/datasources/firebase_auth_datasources.dart';
-import 'package:brainlytic/features/auth/data/repository/auth_repository_impl.dart';
-import 'package:brainlytic/features/auth/domain/usecases/register_usecase.dart';
-import 'package:brainlytic/features/auth/pages/bloc/auth_bloc.dart';
+import 'package:brainlytic/brainlytic.dart';
 import 'package:brainlytic/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -24,43 +18,4 @@ void main() async{
       child: const Brainlytic()
     )
   );
-  
-}
-
-class Brainlytic extends StatelessWidget {
-  const Brainlytic({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MultiBlocProvider(
-      providers: [
-        BlocProvider<AuthBloc>(
-          create: (_)=>AuthBloc(
-            registerUsecase: RegisterUsecase(
-              AuthRepositoryImpl(
-                FirebaseAuthDatasources()
-              )
-            )
-          )
-        )
-      ],
-      child: MaterialApp.router(
-        title: "Brainlytic",
-        debugShowCheckedModeBanner: false,
-        theme: AppTheme.darkTheme,
-        // home: StreamBuilder(
-        //   stream: FirebaseAuth.instance.authStateChanges(),
-        //   builder: (context, snapshot) {
-            
-        //     if(snapshot.data != null){
-        //       return HomeScreen();
-        //     }
-        //     return Onboarding();
-        //   },
-        // )
-      
-        routerConfig: appRouter,
-      ),
-    );
-  }
 }
