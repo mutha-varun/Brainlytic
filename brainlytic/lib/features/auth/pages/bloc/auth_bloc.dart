dc5006e073c847e0006693aca3ff799f18c5e3ec
Varun Mutha <muthavarun07@gmail.com>
2026-07-15 23:20:23 +0530

    Feat: Added login funcionality to bloc

--- brainlytic/lib/features/auth/pages/bloc/auth_bloc.dart
+++ brainlytic/lib/features/auth/pages/bloc/auth_bloc.dart
@@ -1,4 +1,5 @@
 import 'package:brainlytic/core/entities/user.dart';
+import 'package:brainlytic/features/auth/domain/usecases/login_usecase.dart';
 import 'package:brainlytic/features/auth/domain/usecases/register_usecase.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:meta/meta.dart';
@@ -9,25 +10,32 @@ part 'auth_state.dart';
 class AuthBloc extends Bloc<AuthEvent, AuthState> { 
 
   final RegisterUsecase _registerUsecase;
+  final LoginUsecase _loginUsecase;
 
   AuthBloc({
-    required RegisterUsecase registerUsecase
+    required RegisterUsecase registerUsecase,
+    required LoginUsecase loginUsecase
   }) : 
   _registerUsecase = registerUsecase,
+  _loginUsecase = loginUsecase,
   super(AuthInitial()) {
     on<AuthEvent>((_, emit) =>emit(AuthLoading()));
-
-    // on<Login>((event, emit) async {
-
-    //   final res = await 
-
-    //   res.fold
-    //   emit(AuthSuccess());
-    // });
 
     on<RegisterEvent>((event, emit) async {
       final res = await _registerUsecase(
         name: event.name,
+        email: event.email,
+        password: event.password
+      );
+
+      res.fold(
+        (l)=>emit(AuthFailure(l.message)), 
+        (user)=>emit(AuthSuccess(user))
+      );
+    });
+
+    on<LoginEvent>((event, emit) async{
+      final res = await _loginUsecase(
         email: event.email,
         password: event.password
       );
--- brainlytic/lib/features/auth/pages/bloc/auth_event.dart
+++ brainlytic/lib/features/auth/pages/bloc/auth_event.dart
@@ -3,8 +3,14 @@ part of 'auth_bloc.dart';
 @immutable
 sealed class AuthEvent {}
 
-final class Login extends AuthEvent{
+final class LoginEvent extends AuthEvent{
+  final String email;
+  final String password;
 
+  LoginEvent({
+    required this.email,
+    required this.password
+  });
 }
 
 final class RegisterEvent extends AuthEvent{
