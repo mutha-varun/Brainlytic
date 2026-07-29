80b0aa6858bd91a20d00cc651f94b0c3b3c52fe3
Varun Mutha <muthavarun07@gmail.com>
2026-07-30 00:24:31 +0530

    Refactored: Added data fetch event and star update when quiz completed using bloc comsumer

--- brainlytic/lib/features/home/pages/homescreen.dart
+++ brainlytic/lib/features/home/pages/homescreen.dart
@@ -1,80 +1,112 @@
-import 'package:brainlytic/core/router/route_constants.dart';
+import 'package:brainlytic/core/router/app_router.dart';
 import 'package:brainlytic/features/common/widgets/circularindicator.dart';
+import 'package:brainlytic/features/home/data/models/quiz_model.dart';
+import 'package:brainlytic/features/home/pages/bloc/home_bloc.dart';
 import 'package:brainlytic/features/home/pages/widgets/quiz_topics_templates.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomeScreen extends StatefulWidget {
+  final String? userId;
+
   const HomeScreen({
     super.key,
+    this.userId,
   });
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen> with RouteAware{
 
-  final userId = FirebaseAuth.instance.currentUser!.uid;
-  final name = FirebaseAuth.instance.currentUser!.displayName;
+  late final String userId;
 
-  Stream<Map<String, int>> getUserStars(){
-    return FirebaseFirestore.instance.collection('userData').
-    doc(userId).collection('quizData').snapshots().
-    map((snapshot) {
-      final starsMap = <String, int>{};
-      for (var doc in snapshot.docs) {
-        final quizNumber = doc.id.replaceAll('quiz', '');
-        starsMap['quiz$quizNumber'] = doc['stars'] as int;
-      }
-      return starsMap;
-    });
-  }
+//   final userId = FirebaseAuth.instance.currentUser!.uid;
+//   final name = FirebaseAuth.instance.currentUser!.displayName;
+
+//   Stream<Map<String, int>> getUserStars(){
+//     return FirebaseFirestore.instance.collection('userData').
+//     doc(userId).collection('quizData').snapshots().
+//     map((snapshot) {
+//       final starsMap = <String, int>{};
+//       for (var doc in snapshot.docs) {
+//         final quizNumber = doc.id.replaceAll('quiz', '');
+//         starsMap['quiz$quizNumber'] = doc['stars'] as int;
+//       }
+//       return starsMap;
+//     });
+//   }
 
   Color hextoColor(String hex) {
       return Color(int.parse(hex, radix: 16) + 0xFF000000);
   }
 
-  void signOut(){
-    showDialog(context: context, builder: (context) {
-      return AlertDialog(
-        title: Text("Logout",
-          textAlign: TextAlign.center,
-          style: Theme.of(context).textTheme.titleLarge
-        ),
-        content: Text("Are you sure you want to logout?",
-          style: Theme.of(context).textTheme.bodyLarge
-        ),
-        actions: [
-          TextButton(
-            onPressed: () => Navigator.pop(context),
-            child: Text("No",
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Colors.blue
-              )
-            ),
-          ),
-          TextButton(
-            onPressed: () {
-              FirebaseAuth.instance.signOut();
-              context.goNamed(RouteConstants.onboarding);
-            },
-            child:  Text("Logout",
-              style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                color: Colors.red
-              )
-            ),
-          ),
-        ],
-      );
-    });
+//   void signOut(){
+//     showDialog(context: context, builder: (context) {
+//       return AlertDialog(
+//         title: Text("Logout",
+//           textAlign: TextAlign.center,
+//           style: Theme.of(context).textTheme.titleLarge
+//         ),
+//         content: Text("Are you sure you want to logout?",
+//           style: Theme.of(context).textTheme.bodyLarge
+//         ),
+//         actions: [
+//           TextButton(
+//             onPressed: () => Navigator.pop(context),
+//             child: Text("No",
+//               style: Theme.of(context).textTheme.titleSmall!.copyWith(
+//                 color: Colors.blue
+//               )
+//             ),
+//           ),
+//           TextButton(
+//             onPressed: () {
+//               FirebaseAuth.instance.signOut();
+//               context.goNamed(RouteConstants.onboarding);
+//             },
+//             child:  Text("Logout",
+//               style: Theme.of(context).textTheme.titleSmall!.copyWith(
+//                 color: Colors.red
+//               )
+//             ),
+//           ),
+//         ],
+//       );
+//     });
+//   }
+
+
+  @override
+  void didChangeDependencies() {
+    routeObserver.subscribe(this, ModalRoute.of(context) as PageRoute);
+    super.didChangeDependencies();
   }
 
   @override
+  void initState() {
+    super.initState();
+    userId = FirebaseAuth.instance.currentUser!.uid;
+    context.read<HomeBloc>().add(HomeDataFetch(userId));
+  }
+
+  @override
+  void dispose() {
+    routeObserver.unsubscribe(this);
+    super.dispose();
+  }
+
+  @override
+  void didPopNext() {
+    context.read<HomeBloc>().add(StarRefreshed(userId));
+    super.didPopNext();
+  }
+
+
+  @override
   Widget build(BuildContext context) {
-    
     return Scaffold(
       appBar: AppBar(
         title: Padding(
@@ -85,7 +117,7 @@ class _HomeScreenState extends State<HomeScreen> {
               Icon(Icons.home,
                 size: 30,
               ),
-              Text("Hey! $name",
+              Text("Hey!",
                 style: Theme.of(context).textTheme.headlineSmall,
               ),
             ],
@@ -96,48 +128,83 @@ class _HomeScreenState extends State<HomeScreen> {
           IconButton(
             padding: EdgeInsets.only(right: 20),
             icon: const Icon(Icons.logout),
-            onPressed: () => signOut(),
+            onPressed: () {},
           ),
         ],
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          StreamBuilder<Map<String, int>>(
-            stream: getUserStars(),
-            builder: (context, asyncSnapshot) {
-              return StreamBuilder(
-                stream: FirebaseFirestore.instance.collection('quizzes').snapshots(),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return Center(
-                      child: const Circularindicator(),
-                    );
-                  }
-                  return Expanded(
-                    child: ListView.builder(
-                      itemCount: 7,
-                      itemBuilder: (context, index){
-                        final quizData = snapshot.data!.docs[index].data();
-                        final quizId = 'quiz${quizData['id']}';
-                        final userStars  = asyncSnapshot.data?[quizId] ?? 0;
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.center,
+      //   children: [
+        //   StreamBuilder<Map<String, int>>(
+        //     stream: getUserStars(),
+        //     builder: (context, asyncSnapshot) {
+        //       return StreamBuilder(
+        //         stream: FirebaseFirestore.instance.collection('quizzes').snapshots(),
+        //         builder: (context, snapshot) {
+        //           if (snapshot.connectionState == ConnectionState.waiting) {
+        //             return Center(
+        //               child: const Circularindicator(),
+        //             );
+        //           }
+        //           return Expanded(
+        //             child: ListView.builder(
+        //               itemCount: 7,
+        //               itemBuilder: (context, index){
+        //                 final quizData = snapshot.data!.docs[index].data();
+        //                 final quizId = 'quiz${quizData['id']}';
+        //                 final userStars  = asyncSnapshot.data?[quizId] ?? 0;
 
-                        return QuizTopicsTemplates(
-                          color: hextoColor(quizData['color']),
-                          title: quizData['title'],
-                          id: quizData['id'],
-                          stars: userStars,
-                          totalQuestions: quizData['Questions'],
-                        );
-                      },
-                    ),
-                  );
-                }
-              );
-            }
-          ),
-        ],
-      ),
+        //                 return QuizTopicsTemplates(
+        //                   color: hextoColor(quizData['color']),
+        //                   title: quizData['title'],
+        //                   id: quizData['id'],
+        //                   stars: userStars,
+        //                   totalQuestions: quizData['Questions'],
+        //                 );
+        //               },
+        //             ),
+        //           );
+        //         }
+        //       );
+        //     }
+        //   ),
+        // ],
+      //),
+      body: BlocConsumer<HomeBloc, HomeState>(
+        listener: (context, state){
+          if(state is HomeError){
+            
+          }
+        },
+        builder: (context, state){
+
+          if(state is HomeLoading){
+            return Center(
+              child: Circularindicator()
+            );
+          }
+
+          else if(state is HomeDataFetchSuccess){
+            final List<QuizModel> quizzes = state.quizzes;
+            final  stars = state.stars;
+
+            return ListView.builder(
+              itemCount: quizzes.length,
+              itemBuilder: (context, index){
+                final quiz = quizzes[index];
+                return QuizTopicsTemplates(
+                  key: ValueKey(quiz.id),
+                  color: hextoColor(quiz.color), 
+                  title: quiz.title, 
+                  id: quiz.id , 
+                  totalQuestions: quiz.totalQuestions
+                );
+              }
+            );
+          }
+          return Circularindicator();
+        }, 
+      )
     );
   }
 }
